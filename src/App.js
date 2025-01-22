17290a601db8f743546682c916c154de7ea898e1
Md. Atikur Rahman <58600348+atikrpi27@users.noreply.github.com>
2025-01-22 10:04:18 +0000

    data fatching and showing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Users from './components/Users';
 
 const App = () => {
-  // step1 : declare three states here : users, isLoading, error
+  const [users, setUsers] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  // step2 : use useEffect for fetching the data including updating isLoading and error states
+  useEffect(() => {
+    const fetchUsers = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch('https://fakestoreapi.com/users');
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
+    fetchUsers();
+  }, []);
+  
 
   return (
     <div className="container">
       <h1 className="title">Users Management App</h1>
-      {isLoading && <p>Loading users...</p>}
-      {error && <p>{error}</p>}
+      {isLoading && <p className='loading'>Loading users...</p>}
+      {error && <p className='error'>{error.message}</p>}
       {/* step3 : pass the users data to Users component  */}
+      {users && <Users users={users} />}
     </div>
   );
 };
--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const User = ({ id, name, email, phone }) => {
+const User = ({ id,name, username, email, phone }) => {
   return (
     <article className="user">
       <h3>{id}</h3>
-      <h3 className="user__name">{name}</h3>
+      <h3 className="user__fullname">{name.firstname + " " + name.lastname}</h3>
+      <h3 className="user__name">{username}</h3>
       <p className="user__email">{email}</p>
       <a className="user__phone" href={'tel:+' + phone}>
         {phone}
